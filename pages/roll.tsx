2f394a474506963a998acb318f75cb3176b9daa0
Gilad Gray <giladgray@gmail.com>
2020-10-18 00:01:01 -0700

    prettier

--- pages/roll.tsx
+++ pages/roll.tsx
@@ -1,6 +1,6 @@
-import { memo, ReactChild, useCallback, useEffect, useMemo, useReducer, useState } from "react"
-import { initialState, reducer, RollProps } from "../src/reducer";
-import { stringifyRoll, sum } from "../src/roll";
+import { memo, ReactChild, useCallback, useEffect, useMemo, useReducer, useState } from 'react';
+import { initialState, reducer, RollProps } from '../src/reducer';
+import { stringifyRoll, sum } from '../src/roll';
 import styles from '../styles/roll.module.css';
 
 const STORAGE_KEY = 'dice-city-state';
@@ -10,49 +10,71 @@ export default function Roll() {
     // initialize from local storage
     try {
       const data = localStorage?.getItem(STORAGE_KEY);
-      return new Map(data && JSON.parse(data)) as typeof initialState
-    }
-    catch {
+      return new Map(data && JSON.parse(data)) as typeof initialState;
+    } catch {
       return initialState;
     }
   });
 
   useEffect(() => {
     // save locally when updated
-    localStorage?.setItem(STORAGE_KEY, JSON.stringify(Array.from(state)))
-  }, [state])
+    localStorage?.setItem(STORAGE_KEY, JSON.stringify(Array.from(state)));
+  }, [state]);
 
   const [roll, setRoll] = useState('');
   const handleSubmit = useCallback(() => {
-    dispatch({ type: 'parse', roll })
+    dispatch({ type: 'parse', roll });
     setRoll('');
-  }, [roll])
-  const handleRollAll = useCallback(() => dispatch({ type: 'roll-all' }), [])
-  return <div>
-    <header className={styles.header}>
-      <p>
-        <input type="text" placeholder="2d6+5" value={roll} onChange={e => setRoll(e.currentTarget.value)} />
-        <button onClick={handleSubmit}>Add</button><br/>
-      <small>Use "XdY" format to describe rolls.</small>
-      </p>
-      <button onClick={handleRollAll}>Roll all</button>
-    </header>
-    <ul >
-      {Array.from(state.values()).map((h) => <SingleRoll key={h.id} {...h} dispatch={dispatch} />)}
-    </ul>
-  </div>
+  }, [roll]);
+  const handleRollAll = useCallback(() => dispatch({ type: 'roll-all' }), []);
+  return (
+    <div>
+      <header className={styles.header}>
+        <p>
+          <input type="text" placeholder="2d6+5" value={roll} onChange={(e) => setRoll(e.currentTarget.value)} />
+          <button onClick={handleSubmit}>Add</button>
+          <br />
+          <small>Use "XdY" format to describe rolls.</small>
+        </p>
+        <button onClick={handleRollAll}>Roll all</button>
+      </header>
+      <ul>
+        {Array.from(state.values()).map((h) => (
+          <SingleRoll key={h.id} {...h} dispatch={dispatch} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 const SingleRoll = memo<RollProps>(({ id, roll, dice, name, dispatch }) => {
   const els = dice.map(([val, sides], i) => {
-    const classes = [styles.die, val === sides && styles.crit, val === 1 && styles.fail, sides === 1 && styles.static].filter(Boolean).join(" ")
-    return <div className={classes} key={[val, sides, i].join(' ')}><dd>{val}</dd>{sides > 1 && <dt>d{sides}</dt>}</div>
-  })
-  return <li className={styles.roll}>
-    <input type="text" defaultValue={name} placeholder="Name this roll" onBlur={e => dispatch({ type: 'name', id, name: e.currentTarget.value })} /><br />
-    <h4>{stringifyRoll(roll)}</h4>
-    <dl>{els}<span className={styles.total}>= {sum(dice)}</span></dl>
-    <button onClick={() => dispatch({ id, type: 'roll' })}>Roll</button>&nbsp;
-    <button onClick={() => window.confirm('Are you sure?') && dispatch({ id, type: 'delete' })}>Delete</button>
-  </li>
-})
+    const classes = [styles.die, val === sides && styles.crit, val === 1 && styles.fail, sides === 1 && styles.static]
+      .filter(Boolean)
+      .join(' ');
+    return (
+      <div className={classes} key={[val, sides, i].join(' ')}>
+        <dd>{val}</dd>
+        {sides > 1 && <dt>d{sides}</dt>}
+      </div>
+    );
+  });
+  return (
+    <li className={styles.roll}>
+      <input
+        type="text"
+        defaultValue={name}
+        placeholder="Name this roll"
+        onBlur={(e) => dispatch({ type: 'name', id, name: e.currentTarget.value })}
+      />
+      <br />
+      <h4>{stringifyRoll(roll)}</h4>
+      <dl>
+        {els}
+        <span className={styles.total}>= {sum(dice)}</span>
+      </dl>
+      <button onClick={() => dispatch({ id, type: 'roll' })}>Roll</button>&nbsp;
+      <button onClick={() => window.confirm('Are you sure?') && dispatch({ id, type: 'delete' })}>Delete</button>
+    </li>
+  );
+});
--- src/roll.ts
+++ src/roll.ts
@@ -1,13 +1,13 @@
-export type ParsedRoll = { count: number, sides: number }[]
+export type ParsedRoll = { count: number; sides: number }[];
 export type RolledDice = [number, number][];
 
 const diePattern = /^(\d*)(?:[dD](\d+))?$/;
 export function parseDie(roll: string) {
   const [, count, sides] = diePattern.exec(roll) ?? [];
-  return { count: +count || 1, sides: +sides || 1 }
+  return { count: +count || 1, sides: +sides || 1 };
 }
 
-const rollValidator = /^[dD\d\s+]+$/
+const rollValidator = /^[dD\d\s+]+$/;
 const rollSplitter = /\s*\+\s*/;
 export function parseRoll(roll: string): ParsedRoll | null {
   if (!rollValidator.test(roll)) return null;
@@ -15,22 +15,22 @@ export function parseRoll(roll: string): ParsedRoll | null {
 }
 
 export function stringifyRoll(roll: ParsedRoll) {
-  return roll.map(r => r.sides === 1 ? r.count : `${r.count}d${r.sides}`).join(' + ')
+  return roll.map((r) => (r.sides === 1 ? r.count : `${r.count}d${r.sides}`)).join(' + ');
 }
 
 export function rollDie(die: ParsedRoll[0]): RolledDice {
   if (die.sides === 1) return [[die.count, 1]];
-  const rolls: RolledDice = []
+  const rolls: RolledDice = [];
   for (let i = 0; i < die.count; i++) {
-    rolls.push([1 + Math.floor(Math.random() * die.sides), die.sides])
+    rolls.push([1 + Math.floor(Math.random() * die.sides), die.sides]);
   }
   return rolls;
 }
 
 export function performRoll(roll: ParsedRoll) {
-  return roll.reduce<RolledDice>((acc, r) => acc.concat(rollDie(r)), [])
+  return roll.reduce<RolledDice>((acc, r) => acc.concat(rollDie(r)), []);
 }
 
 export function sum(rolls: RolledDice) {
-  return rolls.reduce<number>((acc, r) => acc += r[0], 0)
+  return rolls.reduce<number>((acc, r) => (acc += r[0]), 0);
 }
