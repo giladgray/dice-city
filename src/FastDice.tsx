5be362c6a9bd3e1116ade1c1d8a33481fab018df
Gilad Gray <giladgray@gmail.com>
2020-11-08 12:14:58 -0800

    persist FastDice to local storage

--- src/FastDice.tsx
+++ src/FastDice.tsx
@@ -1,22 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../styles/roll.module.css";
 import { OneRoll } from "./OneRoll";
+import { useLocalStorage } from "./useLocalStorage";
 
 const BASIC_ROLLS = ["d4", "d6", "d10", "d20", "d100"];
+const PROMPT_MSG = "Enter new Fast Dice rolls, separated by commas.\nEmpty text will reset to default.";
+const STORAGE_KEY = "dice-city-fast-dice";
 
 export const FastDice: React.FC = () => {
-  const [fastDice, setFastDice] = useState(BASIC_ROLLS);
+  const [fastDice, setFastDice] = useLocalStorage(STORAGE_KEY, BASIC_ROLLS);
+
   function handleEdit() {
-    const newDice = window.prompt("Enter new Fast Dice, separated by commas:", fastDice.join(", "));
-    if (newDice) {
-      setFastDice(
-        newDice
-          .split(",")
-          .map((s) => s.trim())
-          .filter(Boolean)
-      );
-    }
+    const response = window.prompt(PROMPT_MSG, fastDice.join(", "));
+    // cancel => noop
+    if (response == null) return;
+    // empty input => reset to default
+    if (!response.trim()) return setFastDice(BASIC_ROLLS);
+    // otherwise, update state
+    const newDice = response
+      .split(",")
+      .map((s) => s.trim())
+      .filter(Boolean);
+    setFastDice(newDice.map((s) => s.trim()).filter(Boolean));
   }
+
   return (
     <div className={styles.top}>
       <h4>
