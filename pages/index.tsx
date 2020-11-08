3d148fce7eb48164014a3c34c458ce5bebc0b475
Gilad Gray <giladgray@gmail.com>
2020-11-08 12:28:49 -0800

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,10 @@ export default function Roll(): JSX.Element {
   return (
     <div>
       <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <title>Dice City</title>
       </Head>
       <header className={styles.header}>
@@ -43,3 +47,5 @@ export default function Roll(): JSX.Element {
     </div>
   );
 }
+
+// Icons made by <a href="https://www.flaticon.com/authors/alfredo-hernandez" title="Alfredo Hernandez">Alfredo Hernandez</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
