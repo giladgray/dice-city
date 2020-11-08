bcd4be7046c4e2a53c7f9dbbbb523a623dc1b323
Gilad Gray <giladgray@gmail.com>
2020-11-08 11:43:53 -0800

    NewRoll is a form

--- src/NewRoll.tsx
+++ src/NewRoll.tsx
@@ -9,25 +9,28 @@ export const NewRoll = memo<Props>(({ onCreate }) => {
   const name = useInput("Name new roll");
   const roll = useInput("1d12+6");
 
-  function handleCreate() {
-    onCreate(roll.value.trim(), name.value.trim());
-    name.onChange("");
-    roll.onChange("");
+  function handleCreate(e: React.FormEvent) {
+    e.preventDefault();
+    if (roll.value) {
+      onCreate(roll.value.trim(), name.value.trim());
+      name.onChange("");
+      roll.onChange("");
+    }
   }
 
   return (
-    <div className={styles.roll}>
+    <form className={styles.roll} onSubmit={handleCreate}>
       <input type="text" {...name} />
       <dl className={styles.flexRow}>
         <div className={styles.die + " " + styles.static}>
           <dt>
             Describe rolls in <em>XdY+Z</em> format.
           </dt>
-          <input type="text" {...roll} />
+          <input type="text" pattern="[d+-\\d\\s]*" {...roll} />
         </div>
       </dl>
-      <button onClick={handleCreate}>Add roll</button>
-    </div>
+      <button type="submit">Add roll</button>
+    </form>
   );
 });
 NewRoll.displayName = "NewRoll";
