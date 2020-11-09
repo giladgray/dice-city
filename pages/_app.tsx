7deed254ef03b4adfe16a35b05e10e1dd6211792
Gilad Gray <giladgray@gmail.com>
2020-11-08 23:08:53 -0800

    add MotionConfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+import { AnimationFeature, MotionConfig } from "framer-motion";
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <MotionConfig features={[AnimationFeature]}>
+      <Component {...pageProps} />;
+    </MotionConfig>
+  );
 }
 
 export default MyApp;
